76a405932999f7945ebffa2eebdb4297cbed6028
khadyothan <kdasari1@asu.edu>
2024-07-28 17:47:05 -0700

    removed the abstraction for eventEmitter

--- 2.5 Express Server From Scratch/express.js
+++ 2.5 Express Server From Scratch/express.js
@@ -4,19 +4,16 @@ import http from "http";
 const EventEmitter = events.EventEmitter();
 
 function express() {
-  const app = function (req, res) {
-    app.handle(req, res);
-  };
-
+  const app = function () {};
   Object.assign(app, proto);
   return app;
 }
 
-const proto = Object.create(events.EventEmitter.prototype);
-
-proto.listen = function (port, callback) {
-  const server = http.createServer(this);
-  return server.listen(port, callback);
+const proto = {
+  listen: function (port, callback) {
+    const server = http.createServer(this);
+    return server.listen(port, callback);
+  },
 };
 
 export { express };
